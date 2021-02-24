f719fdf5c1e9376e6bc4036d5d65233b532897cc
kgrose102 <kgrose102@gmail.com>
2021-02-24 14:48:18 -0500

    Audio controls added

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@
 (() =>{
   // select and work with out audio element
   console.log('fired!');
+  // console.log(50/100);
 
   // Variables at the top
 
   let theAudio = document.querySelector("audio"),
-      playButton = document.querySelector("button"),
+      controlButtons = document.querySelectorAll("button"),
       albumArt = document.querySelectorAll(".track-ref")
 
   // functions in the middle
-  function playTrack(){
+  function loadTrack(){
     // debugger;
     // grab value of the custom data atrribue
     let targetTrack = this.dataset.trackref;
@@ -18,16 +19,37 @@
     // load the new audio source
     theAudio.src = `audio/${targetTrack}.mp3`;
 
-    // theAudio.volume = 0.5;
+    theAudio.volume = 0.5;
 
     // load and play the new source
     theAudio.load();
+    // theAudio.play();
+    // the above function is inside the playTrack function
+    playTrack();
+  }
+  function rewindTrack(){
+    // rewind the current track back to the beginning
+    theAudio.currentTime = 0;
+  }
+
+  function stopTrack(){
+    theAudio.pause();
+    // "method" and built in to JavaScript
+  }
+
+  function playTrack(){
     theAudio.play();
   }
+
 
   // event handling at the bottom
   for (cover of albumArt){
     // debugger;
-    cover.addEventListener("click", playTrack);
+    cover.addEventListener("click", loadTrack);
   }
+
+  // get the play button -> the first element in the collection
+  controlButtons[0].addEventListener("click", playTrack);
+  controlButtons[1].addEventListener("click", stopTrack);
+  controlButtons[2].addEventListener("click", rewindTrack);
 })()
